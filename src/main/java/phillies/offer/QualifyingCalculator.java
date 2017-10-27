5934003a580f7b320449ecc5caa44bcef1f90876
spags <james.spagnola@salesforce.com>
2017-10-26 21:58:13 -0700

    add comparable salary information

--- src/main/java/phillies/offer/QualifyingCalculator.java
+++ src/main/java/phillies/offer/QualifyingCalculator.java
@@ -1,6 +1,10 @@
 package phillies.offer;
 
+import com.google.common.base.Preconditions;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Determines a qualifying offer.
@@ -21,11 +25,12 @@ class QualifyingCalculator {
 
     /**
      * Given a list of players calculates the qualifying based on the average of the top 150 players salaries.
+     * <p>
+     * This assumes the list of players is already sorted in descending order of salary.
      */
     long calculate(final List<Player> players) {
-        // For simplicity this is assuming the list of players is modifiable.
-        // Although in general I would avoid that assumption.
-        players.sort(Player.BY_SALARY.reversed());
+        Objects.requireNonNull(players, "list of players must be non null");
+        Preconditions.checkArgument(!players.isEmpty(), "list of players must be non empty");
         // If the number of players is less than 150, than take all available players.
         final int size = Math.min(150, players.size());
         long total = 0;
@@ -37,4 +42,19 @@ class QualifyingCalculator {
         return total / size;
     }
 
+    int findMiddle(final long result, final List<Player> players) {
+        Objects.requireNonNull(players, "list of players must be non null");
+        Preconditions.checkArgument(!players.isEmpty(), "list of players must be non empty");
+
+        // Because its an average the result must somewhere within the list of players.
+        // That is we can start the first player and end at the last player and don't need special edge cases.
+        final int size = players.size() - 1;
+        // We could do a binary search but for simplicity the search is over at most 150 players so small enough to not care.
+        for (int i = 0; i < size; i++) {
+            if (players.get(i).getSalary() >= result && result >= players.get(i + 1).getSalary()) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("result was not within the player range: " + result);
+    }
 }
--- src/main/java/phillies/offer/Runner.java
+++ src/main/java/phillies/offer/Runner.java
@@ -12,9 +12,26 @@ public class Runner {
 
     public static void main(final String... args) {
         final List<Player> players = new Scraper().scrape();
+        players.sort(Player.BY_SALARY.reversed());
         final long offer = QualifyingCalculator.get().calculate(players);
         final String value = NumberFormat.getCurrencyInstance().format(offer);
+        final int middle = QualifyingCalculator.get().findMiddle(offer, players);
 
-        System.out.println("salary: " + value);
+        // Give extra information for comparable players' salaries.
+        // Here I decided at most 5 above and 5 below the qualifying offer.
+        final int top = Math.max(middle - 5, 0);
+        System.out.println((middle - top) + " closet players above the qualifying offer:");
+        for (int i = top; i <= middle; i++) {
+            final Player player = players.get(i);
+            System.out.println(player.getName() + " " + NumberFormat.getCurrencyInstance().format(player.getSalary()));
+        }
+        // We don't want to count the middle in the low range so we modify 1 as appropriate.
+        final int bottom = Math.min(middle + 5 + 1, players.size());
+        System.out.println("\n" + (bottom - middle - 1) + " closet players below the qualifying offer:");
+        for (int i = middle + 1; i < bottom; i++) {
+            final Player player = players.get(i);
+            System.out.println(player.getName() + " " + NumberFormat.getCurrencyInstance().format(player.getSalary()));
+        }
+        System.out.println("\nQualifying Offer Salary: " + value);
     }
 }
